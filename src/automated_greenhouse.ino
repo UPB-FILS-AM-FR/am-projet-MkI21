9d48e8d7c9df44a5cbf5b82c01734c8c13a4cf8e
Tachet Mickael-Eric <119680825+MkI21@users.noreply.github.com>
2024-05-26 14:38:06 +0000

    codul 🥺

--- src/automated_greenhouse.ino
+++ src/automated_greenhouse.ino
@@ -1,10 +1,10 @@
 #include <LCD_I2C.h>
-LCD_I2C lcd(0x27, 16, 2);  // Default address of most PCF8574 modules, change according
+LCD_I2C lcd(0x3F , 16, 2);  // Default address of most PCF8574 modules, change according
 
 #define GLED 3
 #define RLED 2
 #define PUMP 12
-
+//#define PUMP2 11
 #define PLUS_BUTTON 5
 #define MIN_BUTTON 4
 
@@ -41,10 +41,13 @@ void setup() {
   pinMode(GLED, OUTPUT);
   pinMode(RLED, OUTPUT);
   pinMode(PUMP, OUTPUT);
-
+//  pinMode(PUMP2, OUTPUT);
+  
   digitalWrite(GLED, LOW);
   digitalWrite(RLED, LOW);
   digitalWrite(PUMP, LOW);
+  //digitalWrite(PUMP2, LOW);
+
   //senzor hum
   pinMode(HUM, INPUT);
   pinMode(HUM_POWER, OUTPUT);
@@ -60,49 +63,71 @@ void setup() {
 }
 
 void loop() {
+
   int hum = readHum();
   int level = readLevel();
-  char sHum[16] = "";
-  //convert int in str
-  sprintf(sHum, "H:%2d%%  ", hum);
 
-  if (level < 50 || (isnan(hum) || isnan(level))) {  // Is Not A Number
-    //verif daca senzorele merg
+  if (isnan(hum) || isnan(level)) {  // If either sensor reading is Not a Number
     lcd.clear();
-    lcd.setCursor(0, 0);  //  setting the cursor in the desired position.
-    lcd.print("check senzors");
-    lcd.setCursor(1, 0);  //  setting the cursor in the desired position.
-    lcd.print("is there water??");
+    lcd.setCursor(0, 0);  // Setting the cursor in the desired position.
+    lcd.print("Check sensors");
+
+    lcd.setCursor(0, 1);  // Setting the cursor in the desired position.
+    lcd.print("??");
+
     digitalWrite(PUMP, LOW);
+    //digitalWrite(PUMP2, LOW);
     digitalWrite(RLED, HIGH);
     digitalWrite(GLED, LOW);
-    while (level < 50 || (isnan(hum) || isnan(level))) {
-        delay(1);
+  } else {
+    char sHum[16];
+    sprintf(sHum, "H:%2d%%  ", hum);
+
+    if (level > 50) {
+      digitalWrite(RLED, LOW);
+      digitalWrite(GLED, HIGH);
+
+      lcd.clear();
+      lcd.setCursor(0, 0);  // Setting the cursor in the desired position.
+      lcd.print(sHum);
+
+      lcd.setCursor(1, 0);  // Setting the cursor in the desired position.
+      lcd.print("Target:");
+      lcd.print(target_hum);
+
+      lcd.setCursor(0, 1);  // Setting the cursor in the desired position.
+      lcd.print("Crrnt hum:");
+      lcd.print(map(hum, 0, 1023, 100, 0));
+
+      if (digitalRead(PLUS_BUTTON) == LOW && target_hum < 100) {
+        target_hum += 5;
+        delay(200);  // Debounce delay
       }
+
+      if (digitalRead(MIN_BUTTON) == LOW && target_hum > 0) {
+        target_hum -= 5;
+        delay(200);  // Debounce delay
+      }
+
+      if (map(hum, 0, 1023, 100, 0) > target_hum)
+       { digitalWrite(PUMP, LOW);
+ //       digitalWrite(PUMP2, LOW);
+       }
+      else
+       { digitalWrite(PUMP, HIGH);
+ //       digitalWrite(PUMP2, HIGH);
+       }
+    } else {
+      lcd.clear();
+      lcd.setCursor(0, 1);  // Setting the cursor in the desired position.
+      lcd.print("Is there water??");
+      digitalWrite(PUMP, LOW);
+//      digitalWrite(PUMP2, LOW);
+      digitalWrite(RLED, HIGH);
+      digitalWrite(GLED, LOW);
+
+    }
   }
-  digitalWrite(RLED, LOW);
-  digitalWrite(GLED, HIGH);
 
-  lcd.clear();
-  lcd.setCursor(0, 0);  //  setting the cursor in the desired position.
-  lcd.print(sHum);
-
-  lcd.setCursor(1, 0);  //  setting the cursor in the desired position.
-  lcd.print("target:");
-  lcd.print(target_hum);
-
-  if (digitalRead(PLUS_BUTTON) == LOW && target_hum < 100) {
-    target_hum += 5;
-  }
-
-  if (digitalRead(MIN_BUTTON) == LOW && target_hum > 0) {
-    target_hum -= 5;
-  }
-
-  if (map(hum, 0, 1023, 100, 0) > target_hum)
-    digitalWrite(PUMP, LOW);
-  else
-    digitalWrite(PUMP, HIGH);
-
-  delay(1000);
+  delay(300);  // Delay to reduce the update rate of the LCD and sensor reads
 }
